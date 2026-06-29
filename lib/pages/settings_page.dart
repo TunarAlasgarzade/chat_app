1770812f1f519af6a0e9904c14c792bba5aed70e
Tunar <jasminbridgeeton@gmail.com>
2026-06-29 13:51:29 +0400

    feat: rename app to Messenger

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,5 @@
 import 'package:chat_app/pages/appearance_page.dart';
-import 'package:chat_app/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'blocked_users_page.dart';
 
